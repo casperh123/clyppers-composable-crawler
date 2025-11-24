c909668a78cd89e4aea423c5b1cc37e01ceda250
Casper Holten <casperholten@protonmail.com>
2025-11-24 11:36:50 +0100

    Add tests for Deduplication and CompositeFilter

--- test/Crawler.Core.Tests/FilterTests/DeduplicationFilterTests.cs
+++ test/Crawler.Core.Tests/FilterTests/DeduplicationFilterTests.cs
@@ -10,8 +10,8 @@ public class DeduplicationFilterTests
     [Fact]
     public void ShouldCrawl_ReturnsTrue_WhenUriNotCrawled()
     {
-        Uri uri = new Uri("http://samesite.com/page");
-        Uri referringUri = new Uri("http://samesite.com/otherpage");
+        Uri uri = new Uri("https://samesite.com/page");
+        Uri referringUri = new Uri("https://samesite.com/otherpage");
         CrawlContext context = new CrawlContext { Uri = uri, ReferringUri = referringUri };
 
         bool shouldCrawl = _sut.ShouldCrawl(context);
@@ -22,12 +22,13 @@ public class DeduplicationFilterTests
     [Fact]
     public void ShouldCrawl_ReturnsFalse_WhenUriAlreadyCrawled()
     {
-        Uri uri = new Uri("http://samesite.com/page");
-        Uri referringUri = new Uri("http://samesite.com/page");
+        Uri uri = new Uri("https://samesite.com/page");
+        Uri referringUri = new Uri("https://samesite.com/page");
         CrawlContext context = new CrawlContext { Uri = uri, ReferringUri = referringUri };
 
-        bool shouldCrawl = _sut.ShouldCrawl(context);
+        _sut.ShouldCrawl(context);
+        bool crawledDuplicate = _sut.ShouldCrawl(context);
         
-        Assert.True(shouldCrawl);
+        Assert.False(crawledDuplicate);
     }
 }
