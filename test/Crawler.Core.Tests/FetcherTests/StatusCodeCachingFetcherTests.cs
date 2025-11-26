5ea2a4c2587aebd9c22d903de80704c49fa45128
Casper Holten <casperholten@protonmail.com>
2025-11-26 22:59:06 +0100

    Add StatusCodeCachingFetcher tests

---
+++ test/Crawler.Core.Tests/FetcherTests/StatusCodeCachingFetcherTests.cs
@@ -0,0 +1,48 @@
+using System.Net;
+using Crawler.Fetchers;
+using Crawler.Models;
+
+namespace Crawler.Core.Tests.FetcherTests;
+
+public class StatusCodeCachingFetcherTests
+{
+    private readonly StatusCodeCachingFetcher _sut;
+
+    public StatusCodeCachingFetcherTests()
+    {
+        HttpClient client = new HttpClient();
+        _sut = new StatusCodeCachingFetcher(client);
+    }
+
+    [Fact]
+    public async Task FetchAsync_Returns200()
+    {
+        Uri uri = new Uri("https://crawler-test.com/status_codes/status_200");
+        CrawlContext context = new CrawlContext { Uri = uri };
+
+        FetchResult result = await _sut.FetchAsync(context);
+        
+        Assert.True(result.Success);
+        Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+    }
+
+    [Fact]
+    public async Task FetchAsync_WhenStatusCodeSeen_ReturnsCachedStatusCode()
+    {
+        Uri uri = new Uri("https://crawler-test.com/status_codes/status_200");
+        CrawlContext context = new CrawlContext { Uri = uri };
+
+        FetchResult uncachedResult = await _sut.FetchAsync(context);
+        FetchResult cachedResult = await _sut.FetchAsync(context);
+
+        byte[]? uncachedResultContent = uncachedResult.ReadAsByteArray();
+        byte[]? cachedResultContent = cachedResult.ReadAsByteArray();
+
+        Assert.True(uncachedResult.Success);
+        Assert.True(cachedResult.Success);
+        Assert.NotNull(uncachedResultContent);
+        Assert.NotEmpty(uncachedResultContent);
+        Assert.Null(cachedResultContent);
+    }
+    
+}
