acc32a60286a4376756f72940e983ea94ff93840
Casper Holten <casperholten@protonmail.com>
2025-12-01 19:02:11 +0100

    Final Domain Paralllel Parser

--- src/Crawl.Console/Program.cs
+++ src/Crawl.Console/Program.cs
@@ -7,7 +7,22 @@ using Crawl.Filters.InclusionFilters;
 using Crawl.Models;
 using Crawl.Visitors.BrokenLink;
 
-HttpClient client = new HttpClient();
+HttpClientHandler handler = new HttpClientHandler
+{
+    AllowAutoRedirect = true,
+    AutomaticDecompression = System.Net.DecompressionMethods.All,
+};
+
+HttpClient client = new HttpClient(handler);
+
+// Set headers that mimic a normal desktop browser
+client.DefaultRequestHeaders.UserAgent.ParseAdd(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+    "AppleWebKit/537.36 (KHTML, like Gecko) " +
+    "Chrome/123.0.0.0 Safari/537.36");
+
+client.DefaultRequestHeaders.Accept.ParseAdd("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+client.DefaultRequestHeaders.AcceptLanguage.ParseAdd("en-US,en;q=0.9,da;q=0.8");
 
 /*
 Crawl.Core.Crawl crawler = new CrawlerBuilder(client)
@@ -30,6 +45,6 @@ IProgress<CrawlProgress> progress = new Progress<CrawlProgress>(crawlProgress =>
     Console.WriteLine($"Crawling Url: {crawlProgress?.Context?.Uri}, Total crawled: {crawlProgress?.TotalCrawled}, Queue size: {crawlProgress?.QueueSize}");       
 });
     
-Uri uri = new Uri("https://skadedyrsexperten.dk/");
+Uri uri = new Uri("https://www.dr.dk/");
 
 await crawler.CrawlAsync(uri, progress);
